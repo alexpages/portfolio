4058e55e213660640c98173ebcb686e9d66a3d5b
Alex Pages <alexpagesandreu@gmail.com>
2022-06-15 19:08:16 +0200

    Modified header: 1- Modified spaces between elements within header 2- Modified font-size so it looks better 3- Things pending: Icon to be clickable and to redirect to link (anchor), blur on scroll  Tu rama está adelantada a 'origin/main' por 1 commit.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,12 @@
 
 function header(){
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
-            //switch to active class
-            $("header_items").addClass("active");
-        } else {
-            //return to defaul css config
-           $("header_items").removeClass("active");
-        }
-    });
-}
+    $(window).scroll(function(e) {
 
+        var distanceScrolled = $(this).scrollTop();
+    
+        $('.menu_container').css('-webkit-filter', 'blur('+distanceScrolled/60+'px)');
+    
+    })
+};
 
 header();
