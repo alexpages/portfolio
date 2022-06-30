41102eb1902ada5d0af250aa9aa3b07aa33b9112
Alex Pages <alexpagesandreu@gmail.com>
2022-06-30 18:18:47 +0200

    Modification: 1- Added burger button (menu on pressing button yet to be implemented). 2- Modified distribution on header.
     Por favor ingresa el mensaje del commit para tus cambios. Las

--- javascript.js
+++ javascript.js
@@ -6,17 +6,24 @@ function scrollFunction(){
     if (document.body.scrollTop >100 || document.documentElement.scrollTop>100){
         myButton.style.opacity=1;
         myButton.style.cursor="pointer";
-        
     }
     else{
         myButton.style.opacity=0;
         myButton.style.cursor="auto";
-
     }
 }
 
 function topFunction() {
     document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollTop = 0;º
   }
+
+function burger_function() {
+    var x = document.getElementById("nav_items");
+    if (x.style.display=="none"){
+        x.style.display="flex";
+        x.style.flexDirection="column";
+        x.style.height="100%";
+    }
+}
 
