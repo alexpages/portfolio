ffbfcb4c929960aaad0a897f56b017bead5b9ee9
Alex Pages <alexpagesandreu@gmail.com>
2022-07-03 17:00:36 +0200

    Modification: 1- Added button for dark/light theme, function yet to be implemented. 2- Ordered header so the icons do not hide when resizing the screen and show next to the hamburger button 3- Modified some margins of the project content so when screen is smaller the content looks better. 4- Manually centered some contents of the Header and About to look better.

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction(){
     let myButton = document.getElementById("topButton");
     let header = document.getElementsByClassName("nav_bar_container")[0];
+    let button_theme = document.getElementsByClassName("theme")[0];
     
     if (document.body.scrollTop >100 || document.documentElement.scrollTop>100){
         myButton.style.opacity=1;
@@ -13,6 +14,7 @@ function scrollFunction(){
         header.style.position="fixed";
         header.style.backgroundColor = "rgba(30,32,34,.48)";
         header.style.transition="all ease-in 0,2s";
+        button_theme.style.transition="all ease-in 0,2s";
     }
     else{
         myButton.style.opacity=0;
@@ -20,6 +22,9 @@ function scrollFunction(){
         header.style.backgroundColor = "#1E2022";
         header.style.position="static";
         header.style.transition="all ease-in 0,2s";
+
+        button_theme.style.backgroundColor = "transparent";
+        button_theme.style.transition="all ease-in 0,2s";
     }
 
 
@@ -27,7 +32,7 @@ function scrollFunction(){
 
 function topFunction() {
     document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;º
+    document.documentElement.scrollTop = 0;
   }
 
 
