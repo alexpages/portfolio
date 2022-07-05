16228c5aa3465b3e3895a8229b9d03da8bfaa06d
Alex Pages <alexpagesandreu@gmail.com>
2022-07-05 19:21:17 +0200

    Modification: 1- Modified the theme icon so when clicked it changes to the sun, added the hover effects. 2- Added :root.dark and :root.light for the colors. 3- Added JS functions to change the :root class when clicking the theme icon. 4- Reorganized the colors of the different html elements so it works according the html root class.

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function scrollFunction(){
     let header = document.getElementsByClassName("nav_bar_container")[0];
     let button_theme = document.getElementsByClassName("theme")[0];
     
-    if (document.body.scrollTop >100 || document.documentElement.scrollTop>100){
+    if (document.body.scrollTop >15 || document.documentElement.scrollTop>15){
         myButton.style.opacity=1;
         myButton.style.cursor="pointer";
         header.style.position="fixed";
@@ -26,8 +26,6 @@ function scrollFunction(){
         button_theme.style.backgroundColor = "transparent";
         button_theme.style.transition="all ease-in 0,2s";
     }
-
-
 }
 
 function topFunction() {
@@ -40,7 +38,70 @@ function button(){
     const toggleButton = document.getElementsByClassName("toggle_button")[0]
     const navBarLinks = document.getElementsByClassName("nav_items")[0]
     navBarLinks.classList.toggle('active')  
-    
 }
 
+const menuDisplay = () =>{
+    let menu = document.querySelector('.header_container');
 
+    let about = document.getElementById("about");
+    let projects = document.getElementById("projects");
+    let skills = document.getElementById("skills");
+
+    let pos_menu = window.pageYOffset + menu.offsetHeight;
+
+    let pos_about = about.offsetTop + about.offsetHeight;
+    let pos_projects = projects.offsetTop + projects.offsetHeight;
+    let pos_skills = skills.offsetTop + skills.offsetHeight;
+
+    let distance_about = pos_about - pos_menu;
+    let distance_projects = pos_projects - pos_menu;
+    let distance_skills = pos_skills - pos_menu;
+
+    let min = Math.min(...[distance_about, distance_projects, distance_skills].filter(num => num > 0));
+
+    document.querySelectorAll('.icons_header')[3].classList.remove('section_scroll');
+    document.querySelectorAll('.icons_header')[4].classList.remove('section_scroll');
+    document.querySelectorAll('.icons_header')[5].classList.remove('section_scroll');
+
+    if (document.body.scrollTop >15 || document.documentElement.scrollTop>15){
+    if (min === distance_about)
+        document.querySelectorAll('.icons_header')[3].classList.add('section_scroll');
+    if (min === distance_projects)
+        document.querySelectorAll('.icons_header')[4].classList.add('section_scroll');
+    if (min === distance_skills)
+        document.querySelectorAll('.icons_header')[5].classList.add('section_scroll');
+    }
+}
+
+function setTheme(){
+    let html_class_list = document.documentElement.classList;
+
+    if(!html_class_list.contains("dark")){
+        try{
+            html_class_list.replace("light","dark");
+        }
+        finally{
+            html_class_list.add("dark");
+        } 
+    }
+    else{
+        html_class_list.replace("dark","light");
+    }
+
+    console.log(html_class_list);
+
+}
+
+function change_icon(){
+    let theme_icon = document.querySelectorAll(".theme_icon")[0];
+    if (theme_icon.classList.contains("fa-moon")){
+        document.querySelectorAll(".fa-moon")[0].classList.remove("fa-moon");
+        theme_icon.classList.add("fa-sun");
+    }
+    else{
+        document.querySelectorAll(".fa-sun")[0].classList.remove("fa-sun");
+        theme_icon.classList.add("fa-moon");
+    }
+}
+
+window.addEventListener('scroll', menuDisplay);
